b1f68f69a21de8c6b78dfb432a2615344c7baefb
jesh-dev <jeshrunlaw@gmail.com>
2025-08-09 23:57:03 +0100

    feat(PaymentHistory): Add option for 30 items per page in pagination

--- src/AllUsers/UserComponent/PaymentHistory.jsx
+++ src/AllUsers/UserComponent/PaymentHistory.jsx
@@ -160,6 +160,7 @@ export default function PaymentHistory() {
                 <option value={5}>5</option>
                 <option value={10}>10</option>
                 <option value={20}>20</option>
+                <option value={30}>30</option>
               </select>
             </div>
           </div>
